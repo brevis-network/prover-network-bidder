8c676357509757a3cfff71f7a8f5896a5fcc4ed2
Elliott He <elliott.he@outlook.com>
2025-08-12 11:46:00 +0800

    nit refine

--- config/config.go
+++ config/config.go
@@ -8,4 +8,11 @@ type Config struct {
 	BidderEthAddr    string `mapstructure:"bidder_eth_addr"`
 	BidderKeystore   string `mapstructure:"bidder_keystore"`
 	BidderPassphrase string `mapstructure:"bidder_passphrase"`
+
+	MyBidRule MyBidRule `mapstructure:"my_bid_rule"`
+}
+
+// demo purpose, rule params to be determined by business
+type MyBidRule struct {
+	MaxFee string `mapstructure:"max_fee"`
 }
