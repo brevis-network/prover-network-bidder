80907d3d01b66b230b639cf9f1a75077f11427a6
Elliott He <elliott.he@outlook.com>
2025-12-09 10:57:05 +0800

    refine reveal

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -355,13 +355,15 @@ func (s *Scheduler) scheduleReveal() {
 				var jsonErr JsonError
 				errJson, _ := json.Marshal(err)
 				json.Unmarshal(errJson, &jsonErr)
+				var errName string
 				if jsonErr.Data != "" && jsonErr.Data != "0x" {
-					errName, _ := ParseSolCustomErrorName(eth.IBrevisMarketABI, common.FromHex(jsonErr.Data))
+					errName, _ = ParseSolCustomErrorName(eth.IBrevisMarketABI, common.FromHex(jsonErr.Data))
 					errString = errString + " - " + errName
 				}
 				log.Errorf("Reveal req %s err: %s", bid.ReqID, errString)
 
-				if jsonErr.Data != "" && jsonErr.Data != "0x" /*not satisfy contract requirement*/ {
+				if jsonErr.Data != "" && jsonErr.Data != "0x" &&
+					errName != "MarketBiddingPhaseNotEnded" /*not satisfy contract requirement*/ {
 					err = s.UpdateBidAsRevealed(context.Background(), bid.ReqID)
 					if err != nil {
 						log.Errorf("UpdateBidAsRevealed %s err: %s", bid.ReqID, err)
